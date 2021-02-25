445539696058becc220b323c55d74f874884b43c
connorhsm <connorhsmith@gmail.com>
2021-02-25 15:36:32 +1100

    Update remote setup script with new arugments

--- server_admin/setup_remote_valheim_server.sh
+++ server_admin/setup_remote_valheim_server.sh
@@ -6,8 +6,8 @@ echo ""
 echo -n "Enter IP of clean DO ubuntu 20.04 server: "
 read IP
 
-ssh root@${IP} 'bash -s' < setup_ubuntu_20.04.sh
+ssh root@${IP} 'bash -s' < setup_ubuntu_20.04.sh "steam"
 
 ssh steam@${IP} 'bash -s' < install_steamcmd.sh
 
-ssh steam@${IP} 'bash -s' < setup_valheim_server.sh
+ssh steam@${IP} 'bash -s' < setup_valheim_server.sh "My Server" "thepassword"
