9cbcb8fdc19052dc0d02613a71a5e5fee34ab478
connorhsm <connorhsmith@gmail.com>
2021-02-25 15:31:11 +1100

    Remote bash works better now

--- server_admin/setup_remote_valheim_server.sh
+++ server_admin/setup_remote_valheim_server.sh
@@ -6,8 +6,8 @@ echo ""
 echo -n "Enter IP of clean DO ubuntu 20.04 server: "
 read IP
 
-ssh root@${IP} < setup_ubuntu_20.04.sh
+ssh root@${IP} 'bash -s' < setup_ubuntu_20.04.sh
 
-ssh steam@${IP} < install_steamcmd.sh
+ssh steam@${IP} 'bash -s' < install_steamcmd.sh
 
-ssh steam@${IP} < setup_valheim_server.sh
+ssh steam@${IP} 'bash -s' < setup_valheim_server.sh
