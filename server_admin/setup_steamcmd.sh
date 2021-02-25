bfc027807022123933ade65392cf3078c0e2a8d6
connorhsm <connorhsmith@gmail.com>
2021-02-25 14:39:22 +1100

    Remove steamcmd login section

--- server_admin/setup_steamcmd.sh
+++ server_admin/setup_steamcmd.sh
@@ -9,11 +9,3 @@ sudo add-apt-repository multiverse
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt install lib32gcc1 steamcmd -y
-
-# Initiate update and login
-steamcmd
-login blueysmits
-# Password prompt
-# Steamguard prompt
-
-quit
