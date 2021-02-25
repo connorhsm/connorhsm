d9d26828ac76f6547e29078d032192020f86cfe8
connorhsm <connorhsmith@gmail.com>
2021-02-25 14:04:36 +1100

    Steamcmd setup, update and login prompting

--- server_admin/setup_steamcmd.sh
+++ server_admin/setup_steamcmd.sh
@@ -3,7 +3,15 @@
 # Assumed setup_ubuntu_20.04.sh has been run, with user 'steam'
 
 # Source https://developer.valvesoftware.com/wiki/SteamCMD#Linux
+
+# Add package repo and install, make sure to accept the license
 sudo add-apt-repository multiverse
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install lib32gcc1 steamcmd
+sudo apt install lib32gcc1 steamcmd -y
+
+# Initiate update and login
+steamcmd
+login
+# Password prompt
+# Steamguard prompt
