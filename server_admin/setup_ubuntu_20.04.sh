11fe9d114d5e02df5aedcf06f849dcc408d22816
connorhsm <connorhsmith@gmail.com>
2021-02-25 13:36:53 +1100

    Setup script for Ubuntu 20.04

---
+++ server_admin/setup_ubuntu_20.04.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Using Digital Ocean's default Ubuntu 20.04 image
+
+echo -n "Name of sudo user account: "
+read ans
+
+echo "Creating sudo user $ans"
+adduser ${ans}
+usermod
+
+echo -n "Name of the user to create and grant sudo privileges: "
+read USERNAME
+
+# Whether to copy over the root user's `authorized_keys` file to the new sudo
+# user.
+COPY_AUTHORIZED_KEYS_FROM_ROOT=true
+
+# Add sudo user and grant privileges
+useradd --create-home --shell "/bin/bash" --groups sudo "${USERNAME}"
+
+# Check whether the root account has a real password set
+encrypted_root_pw="$(grep root /etc/shadow | cut --delimiter=: --fields=2)"
+
+if [ "${encrypted_root_pw}" != "*" ]; then
+    # Transfer auto-generated root password to user if present
+    # and lock the root account to password-based access
+    echo "${USERNAME}:${encrypted_root_pw}" | chpasswd --encrypted
+    passwd --lock root
+else
+    # Delete invalid password for user if using keys so that a new password
+    # can be set without providing a previous value
+    passwd --delete "${USERNAME}"
+fi
+
+# Expire the sudo user's password immediately to force a change
+chage --lastday 0 "${USERNAME}"
+
+# Create SSH directory for sudo user
+home_directory="$(eval echo ~${USERNAME})"
+mkdir --parents "${home_directory}/.ssh"
+
+# Copy `authorized_keys` file from root if requested
+if [ "${COPY_AUTHORIZED_KEYS_FROM_ROOT}" = true ]; then
+    cp /root/.ssh/authorized_keys "${home_directory}/.ssh"
+fi
+
+# Adjust SSH configuration ownership and permissions
+chmod 0700 "${home_directory}/.ssh"
+chmod 0600 "${home_directory}/.ssh/authorized_keys"
+chown --recursive "${USERNAME}":"${USERNAME}" "${home_directory}/.ssh"
+
+# Disable root SSH login with password
+sed --in-place 's/^PermitRootLogin.*/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
+if sshd -t -q; then
+    systemctl restart sshd
+fi
+
+# Add exception for SSH and then enable UFW firewall
+ufw allow OpenSSH
+ufw --force enable
